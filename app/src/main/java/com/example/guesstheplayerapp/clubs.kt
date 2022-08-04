2b854d3cf798e5bdd586b68c1e6ed2eaf3b648fa
LMBcode <laminmb25@gmail.com>
2022-08-04 11:28:10 +0200

    Initial commit

---
+++ app/src/main/java/com/example/guesstheplayerapp/clubs.kt
@@ -0,0 +1,13 @@
+package com.example.guesstheplayerapp
+
+data class clubs (
+    val club1 : Int,
+    val club2 : Int?,
+    val club3 : Int?,
+    val club4 : Int?,
+    val club5 : Int?,
+    val club6 : Int?,
+    val club7: Int?,
+    val answer : String,
+    val image : Int
+        )
